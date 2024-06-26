8c96d47edd642834d474e5e2ad6a177d2eb3107c
José Crespo <josecrespo00001@gmail.com>
2024-06-26 16:30:31 -0300

    start snippets, assets

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
+
+
+
+
+
+const App = () => {
+
+  return (
+    <main className="gb-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+
+    </main>
+  )
+}
+
+export default App
